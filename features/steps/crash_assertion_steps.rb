8e5f05a47dc52a5669303a5ef1501dfda84f36a8
Delisa Mason <iskanamagus@gmail.com>
2019-07-30 21:38:58 +0100

    tests(maze): Make malloc corruption test compatible with latest SDK

--- features/steps/crash_assertion_steps.rb
+++ features/steps/crash_assertion_steps.rb
@@ -10,12 +10,33 @@ Then("The exception reflects malloc corruption occurred") do
   assert_true(stacktrace.length > 0, "The stacktrace must have more than 0 elements")
 
   case MAZE_SDK
+  when "12.2"
+    assert_exception_matches_12_2(exception, stacktrace)
   when "12.1"
     assert_exception_matches_12_1(exception, stacktrace)
   when "11.2"
     assert_exception_matches_11_2(exception, stacktrace)
   else
     fail("No SDK assertions matching #{MAZE_SDK}")
+  end
+end
+
+def assert_exception_matches_12_2(exception, stacktrace)
+  case stacktrace.first["method"]
+  when "__pthread_kill"
+    assert_equal("SIGABRT", exception["errorClass"])
+    assert_equal("abort", stacktrace[1]["method"])
+  when "nanov2_allocate_from_block"
+    assert_equal("EXC_BAD_INSTRUCTION", exception["errorClass"])
+    assert_equal("nanov2_allocate", stacktrace[1]["method"])
+    assert_equal("NSLog", stacktrace[16]["method"])
+    assert_equal("-[CorruptMallocScenario run]", stacktrace[17]["method"])
+  when "notify_dump_status"
+    assert_equal("EXC_BAD_ACCESS", exception["errorClass"])
+    assert_equal("NSLog", stacktrace[10]["method"])
+    assert_equal("-[CorruptMallocScenario run]", stacktrace[11]["method"])
+  else
+    fail("The exception does not reflect malloc corruption")
   end
 end
 
